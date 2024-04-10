1c8151e9b683d67314539f778e648b5983f84933
Pedrow1406 <150269200+Pedrow1406@users.noreply.github.com>
2024-04-10 19:16:58 -0300

    Aprendendo tipos de variaveis no java

---
+++ src/main/java/com/pedrowsite/introducao/DevPedrow.java
@@ -0,0 +1,15 @@
+package main.java.com.pedrowsite.introducao;
+/*
+Teste de
+Comentários
+em multiplas Linhas
+*/
+
+/**
+ * Isso é um comentário javadoc
+ */
+public class DevPedrow { // Teste de Comentário
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
---
+++ src/main/java/com/pedrowsite/introducao/VariableTypes.java
@@ -0,0 +1,36 @@
+package main.java.com.pedrowsite.introducao;
+
+/**
+ * Existe 8 tipos primitivos
+ * int , double, float, char, byte, short, long, boolean
+ */
+public class VariableTypes {
+    public static void main(String[] args) {
+        // boolean
+        boolean verdadeiro = true; // 1 bit
+        System.out.println(verdadeiro);
+        boolean falso = false; // 1 bit
+        System.out.println(falso);
+        // byte
+        byte numeroByte = -128; // suporta de -128 ate 127 ( 1 Byte || 8 bits)
+        System.out.println(numeroByte);
+        // char
+        char varChar = 'M'; // Aspas Simples ( 2 Bytes || 16 bits)
+        System.out.println(varChar);
+        // short
+        short numeroShort = 9999; // ( 2 Bytes || 16 bits)
+        System.out.println(numeroShort);
+        // int
+        int numeroInt = 999999999; // ( 4 Bytes || 32 bits)
+        System.out.println(numeroInt);
+        // float
+        float numeroFloat = 9.999999999999f; // Sufixo f indica que é um float ( 4 Bytes || 32 bits)
+        System.out.println("Valor float: " + numeroFloat);
+        // double
+        double numeroDouble = 9.999999999999999; // ( 8 Bytes || 64 bits) double é bem mais preciso do que o float
+        System.out.println("Valor double: " + numeroDouble);
+        // long
+        long numeroLong = 999999999999999999L; // Sufixo L indica que é um long (8 Bytes || 64 bits)
+        System.out.println(numeroLong);
+    }
+}
